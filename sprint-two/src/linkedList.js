b967717f91604e55f8cc528dfd3326d2e41a7a3b
csaden <csaden@gmail.com>
2015-07-02 10:56:53 -0700

    Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    if(list.head === null) {
+      list.head = node;
+      list.tail = node;
+      list.head.next = list.tail;
+    } else {
+      var temp = list.tail;
+      temp.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function(){
+    if(list.head !== null) {
+      var oldHead = list.head;
+      list.head = list.head.next;
+      //var oldHead;
+      //list.head = oldHead = list.head.next;
+    }
+    return oldHead.value;
   };
 
   list.contains = function(target){
+    // check head
+    // if target is not list.head.value call list.head.next and check that
+    var res = false;
+    var checkNode = function(node) {
+      if(node !== null && node.value === target){
+        res=true;
+      } else if(node !== null && node.next !== null){
+        checkNode(node.next);
+      }
+    };
+
+    checkNode(list.head);
+    return res;
   };
 
   return list;
