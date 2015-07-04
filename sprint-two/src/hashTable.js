1c9611643103e217d321b2f2d81eb16ceb9040db
csaden <csaden@gmail.com>
2015-07-03 18:15:59 -0700

    Taking git to the next level

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,10 +1,11 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._items = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
-  var j;
+  this._items++;
   var i = getIndexBelowMaxForKey(k, this._limit);
 
   if (this._storage.get(i) === undefined || this._storage.get(i) === null){
@@ -12,6 +13,12 @@ HashTable.prototype.insert = function(k, v){
   	this._storage.set(i, obj);
   }
   this._storage.get(i)[k] = v;
+
+  if (this._items > (this._limit * .75)) {
+    this._limit *= 2;
+    
+
+  }
 
 };
 
@@ -26,11 +33,32 @@ HashTable.prototype.retrieve = function(k){
 };
 
 HashTable.prototype.remove = function(k){
+  this._items--;
 	var i = getIndexBelowMaxForKey(k, this._limit);
 	this._storage.get(i)[k] = null;
 };
 
+HashTable.prototype.createNewStorage = function() {
+  
+};
 
+
+// In Insert
+  // if length.storage > 75%
+  // change limit
+  // call create new storage function
+
+// In remove
+  // if length.storage is less than 75% of half
+  // change limit
+  // call create new storage function
+
+
+// Create New Storage Function
+  // create new storage object
+  // for each item in old storage, 
+    // create new hash
+    // insert into new storage
 
 /*
  * Complexity: What is the time complexity of the above functions?
